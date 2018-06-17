3cda2892bf80d0092afd4bf3bb2fc838dee917ad
jpondy <jpondy@bellsouth.net>
2018-06-17 05:00:19 -0400

    JP

---
+++ src/components/BookList.vue
@@ -0,0 +1,24 @@
+<template>
+    <div><h1>{{title}}</h1></div>
+</template>
+
+<script>
+
+    export default {
+        name: 'BookList',
+        data() {
+            return {
+                title: 'All Books'
+            }
+        }
+    }
+    
+</script>
+
+
+<style>
+    H1, H2 {
+        font-weight: normal
+    }
+</style>
+
