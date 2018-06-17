1cea3a6770084748f27946170f740cfff89aaec7
jpondy <jpondy@bellsouth.net>
2018-06-17 05:27:51 -0400

    JP

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,24 +1,40 @@
 <template>
-    <div><h1>{{title}}</h1></div>
+    <div>
+        <h1>{{title}}</h1>
+        <ul v-for="book in books">
+            <ol>{{book.title}}, {{book.author}}</ol>
+        </ul>
+    </div>
 </template>
 
 <script>
-
-    export default {
-        name: 'BookList',
-        data() {
-            return {
-                title: 'All Books'
-            }
-        }
+export default {
+  name: "BookList",
+  data() {
+    return {
+      title: "All Books",
+      books: 
+      [
+        {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+        {title: 'American Gods', author: 'Neil Gaiman'},
+        {title: 'Amusing Ourselves to Death', author: 'Neil Postman'}
+      ]
     }
-
+  }
+};
 </script>
 
 
 <style>
-    h1, h2 {
-        font-weight: normal
-    }
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+  display: block;
+  margin: 0 10px;
+}
 </style>
 
